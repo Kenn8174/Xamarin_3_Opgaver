7343dba4a52e5ebf528ff0388d658c00c07e99a4
Kenneth Jessen <kennethjessen1@hotmail.com>
2020-11-11 12:40:32 +0100

    Tilføjet Attach Behavior til NewItemPage

---
+++ Xamarin_3_Opgaver/Xamarin_3_Opgaver/Behaviors/CustomAttachBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin_3_Opgaver.Behaviors
+{
+    public static class CustomAttachBehavior
+    {
+        public static readonly BindableProperty AttachBehaviorProperty = BindableProperty.CreateAttached(
+            "AttachBehavior", 
+            typeof(bool), 
+            typeof(CustomAttachBehavior), 
+            false,
+            propertyChanged: OnAttachBehaviorChanged);
+
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+
+        public static void SetAttachBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+        private static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
+        {
+            var entry = view as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.TextChanged += OnEntryTextChanged;
+            }
+            else
+            {
+                entry.TextChanged -= OnEntryTextChanged;
+            }
+        }
+
+        static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
